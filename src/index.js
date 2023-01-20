c621c949e9ee235d89124b2e2744ea5e05e95119
Varvara Gaponova <gaponova.varvara21091995@gmail.com>
2023-01-20 19:18:02 +0400

    feat: finished task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+  const mapper = bracketsConfig.reduce((acc, bracket) => {
+    acc[bracket[1]] = bracket[0];
+    return acc;
+  }, {})
+
+  for (let i = 0; i < str.length; i++) {
+    if(mapper[str[i]] === str[i] && !stack.includes(str[i])) {
+      stack.push(str[i]);
+    } else if(Object.values(mapper).includes(str[i]) && mapper[str[i]] !== str[i]) {
+      stack.push(str[i]);
+    } else if(stack[stack.length - 1] === mapper[str[i]]) {
+      stack.pop();
+    } else return false;
+  }
+
+  return stack.length === 0;
 }
